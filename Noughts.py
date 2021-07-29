ffa84d9879f18167a68ddc35117235fb3274a45f
Nuppiz <nupethenuke@hotmail.com>
2021-07-29 22:45:38 +0300

    Minimax left alone for now, attempting to make mouse controls

--- Noughts.py
+++ Noughts.py
@@ -1,5 +1,4 @@
 import sdl2.ext
-from math import sqrt
 from random import randint
 
 # initialize  SDL2 objects & variables
@@ -31,10 +30,8 @@ def draw(marker, x, y):
     
     draw_x = 105 + (int(x) * 145) # horizontal draw location
     draw_y = 25 + (int(y) * 145) # vertical draw location
-    
-    print("Attempting to draw a", marker, "at", draw_x, ",",draw_y) # debug to check the sprite is loaded in the right spot
 
-    sdl2.SDL_BlitSurface(marker, None, window_surface, sdl2.SDL_Rect(draw_x, draw_y))
+    sdl2.SDL_BlitSurface(marker, None, window_surface, sdl2.SDL_Rect(draw_x, draw_y)) # draw X or O at the correct grid location
 
 class Board:
     
@@ -109,52 +106,86 @@ class Board:
             return 1
         else:
             return 0
+        
+mouseClicked = False
+correctMove = False
+        
+def mouse_processor():
+    
+    global mouseClicked
+    global correctMove
+    
+    mouse_x = 0
+    mouse_y = 0
+    
+    events = sdl2.ext.get_events()
+    
+    for event in events:
+        if event.type == sdl2.SDL_QUIT:
+            return False
+        elif event.type == sdl2.SDL_MOUSEMOTION:
+            mouse_x = event.motion.x
+            mouse_y = event.motion.y
+        elif event.type == sdl2.SDL_MOUSEBUTTONDOWN:
+            if event.button.button == sdl2.SDL_BUTTON_LEFT:
+                mouseClicked = True
+        return mouse_x, mouse_y
+         
+        if int(mouse_x) > 104 and int(mouse_x) < 245:
+            player_col = 0
+            correctMove = True
+        elif int(mouse_x) > 249 and int(mouse_x) < 390:
+            player_col = 1
+            correctMove = True
+        elif int(mouse_x) > 394 and int(mouse_x) < 535:
+            player_col = 2
+            correctMove = True
+        else:
+            print ("Clicked outside of the play area.")
+            
+        if int(mouse_y) > 24 and int(mouse_x) < 165:
+            player_row = 0
+            correctMove = True
+        elif int(mouse_y) > 169 and int(mouse_x) < 310:
+            player_row = 1
+            correctMove = True
+        elif int(mouse_y) > 314 and int(mouse_x) < 455:
+            player_row = 2
+            correctMove = True
+        else:
+            print ("Clicked outside of the play area.")
+            
+        return player_col, player_row
+    
+def player_input(board):
+    
+    global mouseClicked
+    global correctMove
+        
+    while True:
+        mouse_processor()
+        
+        if mouseClicked == True and correctMove == True:
+            # checks if the selected cell is already used up
+            if not Gameboard[player_row][player_col] == "-":
+                print ("Already in use!")
+                mouseClicked = False
+                correctMove = False
+                continue
+            
+            # if all conditions are met, cell is filled with an X
+            else:
+                Gameboard[player_row][player_col] = "X"
+                draw(cross, player_col, player_row)
+                mouseClicked = False
+                correctMove = False
+                window.refresh()
+                break
             
 class Gameplay:
     
     def __init__(self, play_area):
         self.play_area = play_area
-    
-    def player_input(self):
-        
-    # try-excepts to make sure player enters a number and not some other character
-    # column first
-        while True:    
-            try:
-                player_col = int(input("Enter Column: "))-1
-            except ValueError:
-                print ("Please enter a valid number.")
-                continue
-            #checks that player enters a number within the range of the board
-            if not ((player_col >= 0 and player_col < Board.get_cols(self.play_area))):
-                print ("Please enter a valid number.")
-                continue
-            else:
-                break
-            
-        # then row
-        while True:    
-            try:
-                player_row = int(input("Enter Row: "))-1
-            except ValueError:        
-                print ("Please enter a valid number.")
-                continue
-            if not ((player_row >= 0 and player_row < Board.get_rows(self.play_area))):
-                print ("Please enter a valid number.")
-                continue
-            else:
-                break
-    
-        # checks if the selected cell is already used up
-        if not self.play_area.board[player_row][player_col] == "-":
-            print ("Already in use!")
-            self.player_input()
-        
-        # if all conditions are met, cell is filled with an X
-        else:
-            self.play_area.board[player_row][player_col] = "X"
-            draw(cross, player_col, player_row)
-            window.refresh()
             
     def ai_input(self):
         attempts = 0
@@ -179,13 +210,15 @@ class Gameplay:
             break
 
     def ai_move(self):
-        bestScore = 0
+        bestScore = -1000
+        bestMove = None
         
         row = 0
         column = 0
 
-        for column in range(0,len(self.play_area.board[0])):
-            for row in range(0,len(self.play_area.board)):        
+        for column in range(0, 2):
+            column += 1
+            for row in range(0, 2):        
                 if self.play_area.board[row][column] == "-":
                     self.play_area.board[row][column] = "O"
                     score = self.miniMax(self.play_area.board, 0, True)
@@ -193,8 +226,13 @@ class Gameplay:
                     if score > bestScore:
                         bestScore = score
                         self.play_area.board[row][column] = "O"
-            column += 1
-            row += 1
+                        break
+                row += 1
+                
+    scores = {
+        "X": -10,
+        "O": 10,
+        };
                 
     def miniMax(self, board, depth, isMaximizing):
         
@@ -204,43 +242,39 @@ class Gameplay:
         if isMaximizing == True:
             bestScore = -100
             
-            for column in range(0,len(self.play_area.board[0])):
-                for row in range(0,len(self.play_area.board)):  
+            for column in range(0, 2):
+                column += 1
+                for row in range(0, 2):  
             
                     if self.play_area.board[row][column] == "-":
                         self.play_area.board[row][column] = "O"
                         score = self.miniMax(self.play_area.board, depth + 1, False)
                         self.play_area.board[row][column] = "-"
-                        if score > bestScore:
-                            bestScore = score
-                        
-                column += 1
-                row += 1
+                        bestScore = max(score, bestScore)
+                    row += 1
                 
             return bestScore
         
         else:
             bestScore = 100
             
-            for column in range(0,len(self.play_area.board[0])):
-                for row in range(0,len(self.play_area.board)):  
+            for column in range(0, 2):
+                column += 1
+                for row in range(0, 2):  
             
                     if self.play_area.board[row][column] == "-":
                         self.play_area.board[row][column] = "X"
                         score = self.miniMax(self.play_area.board, depth + 1, True)
                         self.play_area.board[row][column] = "-"
-                        if score < bestScore:
-                            bestScore = score
-                            
-                column += 1
-                row += 1
+                        bestScore = min(score, bestScore)
+                    row += 1
                 
             return bestScore
             
     def game_loop(self):
         Gameboard.print_board(Gameboard)
         print ("Player 1's turn")
-        self.player_input()
+        player_input(Gameboard)
         Board.score_checker(Gameboard, "X")
         if Board.score_checker(Gameboard, "X") == 1:
             print ("Player 1 wins!")
@@ -248,7 +282,7 @@ class Gameplay:
             quit()
         Gameboard.print_board(Gameboard)
         print ("AI's turn")
-        self.ai_move()
+        self.ai_input()
         Board.score_checker(Gameboard, "O")
         if Board.score_checker(Gameboard, "O") == 1:
             print ("AI wins!")
@@ -258,12 +292,11 @@ class Gameplay:
 Gameboard = Board(3, 3)   
 Gameplay = Gameplay(Gameboard)
 
-while running == True:
-    # 1. GET INPUT & PROCESS EVENTS
-    events = sdl2.ext.get_events()
-    window.refresh()
-    for event in events:
-      if event.type == sdl2.SDL_QUIT:
-        running = False
-        break
-    Gameplay.game_loop()
+def main():
+    running = True
+    
+    while running == True:
+        window.refresh()
+        Gameplay.game_loop()
+        
+main()
